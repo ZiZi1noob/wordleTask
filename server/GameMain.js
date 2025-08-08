f9b5a1e605240b6ca32102238350f0f32f095232
ZiZi_noOB <purplesunzz@yahoo.com>
2025-08-08 15:15:40 +0800

    receiving console args [-debug / -]d

--- server/GameMain.js
+++ server/GameMain.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
 import { consoleTesting } from "./NormalWordle.js";
 
-consoleTesting();
+// Get command line arguments
+const args = process.argv.slice(2);
+const debugMode = args.includes("--debug") || args.includes("-d");
+
+if (debugMode) {
+  consoleTesting();
+}
